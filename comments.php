3624cbb65bb21c923cf05d9d7a146c2c461a0014
huangpingkang <huangpingkang@mango-go.com>
2019-04-19 17:19:24 +0800

    first commit

---
+++ comments.php
@@ -0,0 +1,223 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php
+header('Access-Control-Allow-Origin:*');
+$submit_ = $_POST['submit'];
+$file_ = $_POST['file'];
+$name_ = $_POST['name'];
+$type_ = $_POST['type'];
+$c_encode = $_GET['text'];
+$dl_ = base64_decode($file_);
+if(strstr($dl_, 'api', TRUE)){ 
+  $file_url = '<font color="blue">评论文件</font>:'.'<a href="'.'https://pan.cccyun.cc/'.strstr($dl_, 'down').'"><font color="red">'.$name_.'</font></a>';
+}
+else if($file_){
+  $file_url = '<font color="blue">评论文件</font>:'.'<a href="'.base64_decode($file_).'"><font color="red">'.$name_.'</font></a>';
+}
+if($c_encode && $file_url){
+  $c_decode = urldecode($c_encode);
+  $file_url = $c_decode.'<br>'.$file_url;
+}
+
+?>
+<?php
+
+
+function threadedComments($comments, $options) {
+    $commentClass = 'commentlist';
+    if ($comments->authorId) {
+        if ($comments->authorId == $comments->ownerId) {
+            $commentClass .= ' comment-by-author';
+        } else {
+            $commentClass .= ' comment-by-user';
+        }
+    }
+
+    $commentLevelClass = $comments->levels > 0 ? ' comment-child' : ' comment-parent';
+
+    if ($comments->url) {
+        $author = '<a href="' . $comments->url . '" target="_blank"' . ' rel="external nofollow">' . $comments->author . '</a>';
+    } else {
+        $author = $comments->author;
+    }
+?>
+
+    <li id="<?php $comments->theId(); ?>" class="comment byuser comment-author-admin bypostauthor depth-<?php echo $comments->levels+1; ?> comment-body<?php
+    if ($comments->levels > 0) {
+        echo ' comment-child';
+        $comments->levelsAlt(' comment-level-odd', ' comment-level-even');
+    } else {
+        echo ' comment-parent';
+    }
+    $comments->alt(' odd', ' even');
+    ?>">
+<?php
+    $host = '//cn.gravatar.com';
+    $url = '/avatar/';
+    $size = '50';
+    $rating = Helper::options()->commentsAvatarRating;
+    $hash = md5(strtolower($comments->mail));
+    $avatar = $host . $url . $hash . '?s=' . $size . '&r=' . $rating . '&d=mm';
+?>
+        <div class="comt-avatar"><img alt="" data-src="<?php echo $avatar; ?>" srcset="<?php echo $avatar; ?> 2x" class="avatar photo" height="50" width="50" src="<?php $aoptions = Typecho_Widget::widget('Widget_Options'); $aoptions ->themeUrl("img/avatar-default.png"); ?>"></div>
+        <div class="comt-main" id="div-<?php $comments->theId(); ?>">
+            <?php $comments->content(); ?>
+            <div class="comt-meta">
+                <span class="comt-author"><?php echo $author; ?></span> <?php $comments->date('Y-m-d'); ?>&nbsp;&nbsp;&nbsp;&nbsp;<?php $comments->reply('回复'); ?>
+            </div>
+        </div>
+    <?php if ($comments->children) { ?><ul class="children"><?php $comments->threadedComments($options); ?></ul><?php } ?>
+    </li><?php } ?>
+<?php $this->comments()->to($comments); ?>
+  <?php if($this->commentsNum>0 || $this->allow('comment')): ?>
+    <div class="title" id="comments">
+        <h3>评论 <?php $this->commentsNum('<small></small>', '<b>1</b>', '<b>%d</b>'); ?></h3>
+    </div>
+  <?php endif; ?>
+    <div id="<?php $this->respondId(); ?>" class="no_webshot">
+
+<?php if(!$this->allow('comment')){ ?>
+        <!-- <h3 class="title">
+             <strong>文章评论已关闭！</strong>
+             </h3> -->
+<?php }else{ ?>
+
+        <form action="<?php $this->commentUrl() ?>" method="post" id="commentform">
+            <div class="comt">
+                <div class="comt-title">
+<?php if($this->user->hasLogin()): ?>
+                    <img alt="" data-src="//cn.gravatar.com/avatar/<?php echo md5($this->user->mail); ?>?s=50" srcset="//cn.gravatar.com/avatar/<?php echo md5($this->user->mail); ?>?s=50 2x" class="avatar photo" height="50" width="50" src="<?php $aoptions = Typecho_Widget::widget('Widget_Options'); $aoptions ->themeUrl("img/avatar-default.png"); ?>">
+
+<?php elseif($this->remember('author',true) != "" && $this->remember('mail',true) != ""): ?>
+                    <img alt="" data-src="//cn.gravatar.com/avatar/<?php echo md5($this->remember('mail')); ?>?s=50" srcset="//cn.gravatar.com/avatar/<?php echo md5($this->remember('mail')); ?>?s=50 2x" class="avatar photo" height="50" width="50" src="<?php $aoptions = Typecho_Widget::widget('Widget_Options'); $aoptions ->themeUrl("img/avatar-default.png"); ?>">
+<?php else: ?><?php $aoptions = Typecho_Widget::widget('Widget_Options');?>
+                    <img alt="" data-src="<?php $aoptions ->themeUrl("img/avatar-default.png"); ?>" srcset="<?php $aoptions ->themeUrl("img/avatar-default.png"); ?> 2x" class="avatar photo" height="50" width="50" src="<?php $aoptions ->themeUrl("img/avatar-default.png"); ?>">
+<?php endif; ?>
+
+                   <p><?php $comments->cancelReply('<font color="##FF5151"><br>取消</font>'); ?></p>
+                </div>
+                <div class="comt-box">
+<font size="3" color="#66B3FF">
+<?php if($this->user->hasLogin()): ?>
+                    <?php $this->user->screenName(); ?>
+                   <?php elseif($this->remember('author',true) != "" && $this->remember('mail',true) != ""): ?><?php $this->remember('author'); ?><?php endif; ?>
+</font>
+                    <textarea placeholder="欢迎在此评论， be nice & cool." class="input-block-level comt-area" name="text" id="comment" cols="100%" rows="3" tabindex="1" ><?php echo $file_url; ?></textarea>
+
+                    <div class="comt-ctrl">
+                        <div class="comt-tips"></div>
+                        <button type="submit" name="submit" id="submit" tabindex="5">提交评论</button>
+<?php $security = $this->widget('Widget_Security'); ?>
+                        <input type="hidden" name="_" value="<?php echo $security->getToken($this->request->getReferer())?>">
+                    </div>
+
+                </div>
+<?php if(!$this->user->hasLogin() && ($this->remember('author',true) == "" || $this->remember('mail',true) == "")): ?>
+                <div class="comt-comterinfo" id="comment-author-info" >
+                    <ul>
+                        <li class="form-inline"><label class="hide" for="author">昵称</label><input class="ipt" type="text" name="author" id="author" value="<?php $this->remember('author'); ?>" tabindex="2" placeholder="昵称（必填）" required><span class="text-muted"></span></li>
+                        <li class="form-inline"><label class="hide" for="mail">邮箱</label><input class="ipt" type="text" name="mail" id="mail" value="<?php $this->remember('mail'); ?>" tabindex="3" placeholder="邮箱（必填）" <?php if ($this->options->commentsRequireMail): ?> required<?php endif; ?>><span class="text-muted"></span></li>
+                        <li class="form-inline"><label class="hide" for="url">网址</label><input class="ipt" type="text" name="url" id="url" value="<?php $this->remember('url'); ?>" tabindex="4" placeholder="网址"><span class="text-muted"></span></li>
+                    </ul>
+                </div>
+<?php endif; ?>
+            </div>
+        </form>
+
+
+<form id="tf" action="https://pan.cccyun.cc/api.php?backurl=" method="post" enctype="multipart/form-data" onsubmit="return beforeSubmit();">
+<input type="file" name="file" style="display:inline-block;margin-left:auto;height:30px;width:auto;">
+<input type="submit" style="float:right;" value="上传">
+</form>
+
+
+
+<?php } ?>
+    </div>
+<?php if ($comments->have()) : ?>
+    <div id="postcomments">
+        <ol class="commentlist"><?php $comments->listComments(array('before' =>  '','after'  =>  '')); ?></ol>
+        <div class="pagination">
+            <?php $comments->pageNav('←','→',2,'...',array('wrapTag' => 'ul')); ?>
+        </div>
+    </div>
+<?php endif; ?>
+
+<script type="text/javascript" src="https://hpk.me/js_own/jquery.base64.js"></script>
+<script>
+function beforeSubmit(){
+    var f = document.getElementById("tf");
+    var nurl = window.location.href.split("#")[0];
+    var url = nurl.split("?")[0]+ "?text=" + encodeURIComponent(document.getElementById("comment").value);
+    f.action = f.action + $.base64.encode(url) ;  
+    return true;
+    //f.submit();          
+}
+
+function showhidediv(id){  
+var sbtitle=document.getElementById(id);  
+if(sbtitle){  
+   if(sbtitle.style.display=='block'){  
+   sbtitle.style.display='none';  
+   }else{  
+   sbtitle.style.display='block';  
+   }  
+}  
+}
+    
+(function () {
+    window.TypechoComment = {
+        dom : function (id) {
+            return document.getElementById(id);
+        },
+        create : function (tag, attr) {
+            var el = document.createElement(tag);
+            for (var key in attr) {
+                el.setAttribute(key, attr[key]);
+            }
+            return el;
+        },
+        reply : function (cid, coid) {
+            var comment = this.dom(cid), parent = comment.parentNode,
+                response = this.dom('<?php echo $this->respondId(); ?>'),
+                input = this.dom('comment-parent'),
+                form = 'form' == response.tagName ? response : response.getElementsByTagName('form')[0],
+                textarea = response.getElementsByTagName('textarea')[0];
+            if (null == input) {
+                input = this.create('input', {
+                    'type' : 'hidden',
+                    'name' : 'parent',
+                    'id'   : 'comment-parent'
+                });
+                form.appendChild(input);
+            }
+            input.setAttribute('value', coid);
+            if (null == this.dom('comment-form-place-holder')) {
+                var holder = this.create('div', {
+                    'id' : 'comment-form-place-holder'
+                });
+                response.parentNode.insertBefore(holder, response);
+            }
+            comment.appendChild(response);
+            this.dom('cancel-comment-reply-link').style.display = '';
+            if (null != textarea && 'text' == textarea.name) {
+                textarea.focus();
+            }
+            return false;
+        },
+        cancelReply : function () {
+            var response = this.dom('<?php echo $this->respondId(); ?>'),
+            holder = this.dom('comment-form-place-holder'),
+            input = this.dom('comment-parent');
+            if (null != input) {
+                input.parentNode.removeChild(input);
+            }
+            if (null == holder) {
+                return true;
+            }
+            this.dom('cancel-comment-reply-link').style.display = 'none';
+            holder.parentNode.insertBefore(response, holder);
+            return false;
+        }
+    };
+})();
+</script><!--</nocompress>-->
---
+++ footer.php
@@ -0,0 +1,60 @@
+<?php
+if (!defined('__TYPECHO_ROOT_DIR__')) exit;
+?>
+
+    <!--footer-->
+<?php if($this->options->footad && !empty($this->options->footad) ): ?>
+    <div class="branding branding-black">
+        <div class="container">
+            <?php $this->options->footad(); ?>
+        </div>
+    </div>
+<?php endif; ?>
+    <footer id="footer" class="footer">
+        <div class="container">
+<?php if($this->options->flinks && !empty($this->options->flinks) ): ?>
+            <div class="flinks">
+                <strong>友情链接</strong>
+                <ul class='xoxo blogroll'>
+                    <?php $this->options->flinks(); ?>
+                </ul>
+            </div>
+<?php endif; ?>
+<?php if($this->options->fcode && !empty($this->options->fcode) ): ?>
+            <div class="fcode"><?php $this->options->fcode(); ?></div>
+<?php endif; ?>
+            <div class="copyright">
+                <p>
+                    &copy; <?php echo date("Y"); ?> <a href="<?php $this->options ->siteUrl(); ?>"><?php $this->options->title();?></a>
+                    <?php if($this->options->miitbeian): ?> | <a href="http://www.miitbeian.gov.cn/" target="_blank" rel="nofollow"><?php $this->options->miitbeian(); ?></a><?php endif; ?>
+                    <br>
+                    Powered by <a href="http://www.typecho.org/" target="_blank">Typecho</a> |
+                    Theme <a href="https://github.com/hpkaiq/Typecho-theme-DUX" target="_blank">DUX</a>
+
+                </p>
+            </div>
+            <div class="hide"></div>
+        </div>
+    </footer>
+<?php if ($this->options->useHighline == 'able'): ?>
+    <script src="//cdn.jsdelivr.net/highlight.js/9.11.0/highlight.min.js"></script>
+<?php endif; ?>
+    <script>
+        window.jsui={
+            www: '<?php $this->options ->siteUrl(); ?>',
+            uri: '<?php echo rtrim($this->options ->themeUrl,"/");?>',
+            ver: '1.0',
+            roll: [<?php if (!empty($this->options->sitebar_fu)) {$text = $this->options->sitebar_fu;} else{$text='1';} $sf_arr = explode(",", $text);foreach($sf_arr as $val){echo '"'.$val.'",';} //侧边栏第几个模块浮动 ?>],
+            ajaxpager: '<?php if ($this->options->infpage == 'disable'): ?>0<?php else: ?>1<?php endif; ?>'
+        };
+    </script>
+
+    <script type='text/javascript' src='//cdn.jsdelivr.net/bootstrap/3.2.0/js/bootstrap.min.js'></script>
+    <script type='text/javascript' src='<?php $this->options->themeUrl('js/loader.js'); ?>'></script>
+    <!-- Analytics code -->
+<?php if($this->options->GoogleAnalytics): ?>
+    <?php $this->options->GoogleAnalytics(); ?>
+<?php endif; ?>
+
+</body>
+</html>
